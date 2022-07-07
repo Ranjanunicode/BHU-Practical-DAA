5903cfc25d66f34d5a921e5d6d6d84577a375991
Ranjan Pandit <59310086+Ranjanunicode@users.noreply.github.com>
2022-07-07 22:16:25 +0530

    Update HeapSortAnalysis.py

--- HeapSortAnalysis.py
+++ HeapSortAnalysis.py
@@ -8,31 +8,24 @@ def heapify(arr, n, i):
     largest = i  
     l = 2 * i + 1
     r = 2 * i + 2	
-
     # See if left child of root exists and is
     # greater than root
     if l < n and arr[largest] < arr[l]:
         largest = l
-
     # See if right child of root exists and is
     # greater than root
     if r < n and arr[largest] < arr[r]:
         largest = r
-
     # Change root, if needed
     if largest != i:
         arr[i], arr[largest] = arr[largest], arr[i]  # swap
-
         heapify(arr, n, largest)
-
 # The main function to sort an array of given size
 def heapSort(arr):
     n = len(arr)
-
     # Build a maxheap.
     for i in range(n//2 - 1, -1, -1):
         heapify(arr, n, i)
-
     # One by one extract elements
     for i in range(n-1, 0, -1):
         arr[i], arr[0] = arr[0], arr[i]  # swap
@@ -48,8 +41,6 @@ def plot(t):
     for i in range(len(n)):
         arr = n[i]*np.log2(n[i]) / 10**5.2
         newArr.append(arr)
-
-
     plt.plot(n, t, label="heap sort")
     plt.plot(n, newArr, label="nlogn")
     plt.xlabel('No. of inputs')
